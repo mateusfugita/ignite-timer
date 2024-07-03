408b2eab8ed5129caf4d5809a5b26e5a3af96fb0
Mateus Fugita <mateushiwao@gmail.com>
2024-07-03 20:52:33 -0300

    feat: create default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from 'styled-components'
 import { Button } from "./components/Button";
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
-    <Button variant="primary" />
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+    </ThemeProvider>
   )
 }
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,9 +17,11 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${props => {
+  background-color: ${props => props.theme[props.variant]};
+
+  /* ${props => {
     return css`
       background-color: ${buttonVariants[props.variant]}
     `
-  }}
+  }} */
 `
