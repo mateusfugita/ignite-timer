66d86f201dc0073bdf8a8feaa52fb4daca9e0d77
Mateus Fugita <mateushiwao@gmail.com>
2024-07-08 20:24:56 -0300

    feat: add dynamic form data type with zod infer method

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,8 +21,10 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -30,7 +32,7 @@ export function Home() {
     },
   })
 
-  function handleCreateNewCycle(data: any){
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
