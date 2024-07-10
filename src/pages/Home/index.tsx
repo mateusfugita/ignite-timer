530939b51d1c6b7b4ee623bff9b423374666542a
Mateus Fugita <mateushiwao@gmail.com>
2024-07-10 20:59:52 -0300

    fix: restart countdown when new cycle is created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -48,12 +48,20 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    // Return serve para limpar a última execução do useEffect
+    // quando há uma atualização no array de dependências
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -67,6 +75,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(newCycle.id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
@@ -79,6 +88,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
